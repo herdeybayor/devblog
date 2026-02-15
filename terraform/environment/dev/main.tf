84f846f3d7aeb8af9ee4e2cb1ddf39fb6745048f
herdeybayor <shereefdeen4real@gmail.com>
2026-02-15 20:59:23 +0100

    feat: initialize Terraform setup with AWS resources, including VPC, EC2 instance, and security group configurations

---
+++ terraform/environment/dev/main.tf
@@ -0,0 +1,34 @@
+# Fetch the latest Ubuntu 24.04 LTS AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_key_pair" "main" {
+  key_name = "${var.project_name}-${var.environment}-key"
+  public_key = file("~/.ssh/devblog.pub")
+  
+  tags = {
+    Name = "${var.project_name}-${var.environment}-key"
+  }
+}
+
+resource "aws_instance" "main" {
+  ami = data.aws_ami.ubuntu.image_id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+  key_name = aws_key_pair.main.key_name
+  
+  tags = {
+    Name = "${var.project_name}-${var.environment}-instance"
+  }
+}
---
+++ terraform/environment/dev/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "The AWS region to deploy the resources"
+  type = string
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "The type of instance to launch"
+  type = string
+  default = "t2.micro"
+}
+
+variable "project_name" {
+  description = "The name of the project"
+  type = string
+  default = "devblog"
+}
+
+variable "environment" {
+  description = "The environment to deploy the resources"
+  type = string
+  default = "dev"
+}
