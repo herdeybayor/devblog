2fba3fa5efdf7267bf11caf8c858ae18f1b3e310
herdeybayor <shereefdeen4real@gmail.com>
2026-02-15 21:54:58 +0100

    feat: enable public IP address association for EC2 instance in Terraform configuration

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -27,7 +27,8 @@ resource "aws_instance" "main" {
   subnet_id = aws_subnet.public.id
   vpc_security_group_ids = [aws_security_group.main.id]
   key_name = aws_key_pair.main.key_name
-  
+  associate_public_ip_address = true
+
   tags = {
     Name = "${var.project_name}-${var.environment}-instance"
   }
